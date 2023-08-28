821b17fef5f61c1666fc624a3635273a1d5b314c
ferelie <abdallah.yasser0501@gmail.com>
2023-08-28 15:34:50 +0300

    re-styled cart button

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import headerImg from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import CartButton from "./CartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Welocme to Abooz's</h1>
+        <CartButton />
       </header>
 
       <div className={classes["main-image"]}>
