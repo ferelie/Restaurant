061c71219d38e6d61253530fb5ac4fff4d10bef7
ferelie <abdallah.yasser0501@gmail.com>
2023-09-08 15:16:19 +0300

    now gets data from backend

--- src/components/Meals/MealsList.jsx
+++ src/components/Meals/MealsList.jsx
@@ -1,10 +1,36 @@
+import React, { useState, useEffect } from "react";
 import classes from "./mealsList.module.css";
-import DUMMY_MEALS from "./dummyList";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 const MealsList = () => {
-  const listOfMeals = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const res = await fetch(
+        "https://restaurant-b7605-default-rtdb.firebaseio.com/meals.json"
+      );
+      const resData = await res.json();
+
+      const loadedMeals = [];
+
+      for (const key in resData) {
+        loadedMeals.push({
+          id: key,
+          name: resData[key].name,
+          description: resData[key].description,
+          price: resData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const listOfMeals = meals.map((meal) => (
     <MealItem key={meal.id} id={meal.id} meal={meal} />
   ));
 
